0d3ed8758b6604b27a861d84ceb3324cbf6cba36
Brian Teddy <teddybrian543@gmail.com>
2025-04-30 12:04:29 +0300

    Add delete notes and refresh text area

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,14 +15,31 @@ function App() {
     console.log(note);
   }
 
+  function deleteNote(id){
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !==id;
+      });
+    })
+
+    console.log("Item Deleted");
+  }
+
   return (
     <div>
       <Header />
       <CreateArea 
         onAdd = {addNote}
       />
-      {notes.map((noteItem) => {
-        return <Note title = {noteItem.title} content = {noteItem.content}/>
+      {notes.map((noteItem, index) => {
+        return (
+          <Note 
+            key = {index}
+            id = {index}
+            title = {noteItem.title} 
+            content = {noteItem.content}
+            onDelete = {deleteNote}
+          />)
       })}
       <Footer />
     </div>
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -21,6 +21,10 @@ function CreateArea(props) {
   function submitNote(event){
     props.onAdd(note);
     event.preventDefault();
+    setNote({
+      title : "",
+      content : ""
+    })
   }
 
   return (
