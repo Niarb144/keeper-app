b72274a90853071b7ca738c354a4fb32eab4e5d5
Ted <54852276+Niarb144@users.noreply.github.com>
2025-05-05 16:14:32 +0300

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import BalconyIcon from '@material-ui/icons/Balcony';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        {/* <BalconyIcon /> */}
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
